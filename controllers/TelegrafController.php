92fa48ce9462b95c35be9f288edadd7d825a900b
foulegold_work <foulegold@gmail.com>
2021-05-04 19:54:09 +0700

    telegraph work

--- controllers/TelegrafController.php
+++ controllers/TelegrafController.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace app\controllers;
 
+use yii\web\Controller;
 
-class TelegrafController extends \yii\web\Controller
+class TelegrafController extends Controller
 {
+    public function actionCreateAccount()
+    {
+        return $this->render('create-account');
+    }
+
+    public function actionCreatePage()
+    {
+        return $this->render('create-page');
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
 
 }
